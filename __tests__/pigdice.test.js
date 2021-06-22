ac7e23412c901de55b234a1d61d8f5b4bb185502
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-22 10:47:12 -0700

    add test 2 for Player constructor prototype = pass
    Co-authored-by: Dave Lindqvist <Lindqvist.Dave@gmail.com>

--- __tests__/pigdice.test.js
+++ __tests__/pigdice.test.js
@@ -1,8 +1,22 @@
 import Player from './../src/pigdice.js';
 
 describe('Player', () => {
-  test('Player will make an instance with name', () => {
-    const player = new Player("name");
-    expect(player.name).toEqual("name");
+
+  beforeEach(() => {
+    // player1 = new Player("Carnitas", 0, 0, true, false);
+  })
+
+  test('should correctly create a player object with respective key-values', () => {
+    let player1 = new Player("Carnitas", 0, 0, true, false);
+    expect(player1.name).toEqual("Carnitas");
+    expect(player1.turnScore).toEqual(0);
+    expect(player1.gameScore).toEqual(0);
+    expect(player1.playerTurn).toEqual(true);
+    expect(player1.playerWin).toEqual(false);
   });
+
+  test ('should roll random number and add to turn score if 2-6 or return 0 if 1', () => {
+    let player1 = new Player("Carnitas", 0, 0, true, false);
+    expect(player1.addScore()).toBeLessThan(7);
+  } )
 });
--- src/pigdice.js
+++ src/pigdice.js
@@ -9,16 +9,16 @@ export default function Player(name, turnScore, gameScore, playerTurn, playerWin
   this.playerWin = playerWin;
 }
 
-
 Player.prototype.addScore = function() {
   let diceRoll = Math.floor(Math.random()*(6)+1);
   if (diceRoll !== 1) { 
       this.turnScore += diceRoll;
       return this.turnScore;
-      // if (this.gameScore >= 100);
-      // this.playerWin = true;
+      // if (this.gameScore >= 100) {
+      //   this.playerWin = true;
+      // }
   } else {
     return this.turnScore = 0;
-//     this.playerTurn = false;
+    // this.playerTurn = false;
   }
 }
