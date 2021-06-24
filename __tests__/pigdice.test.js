aba1d86fe77c5a1632de3f0b806a96de8ea994ac
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-23 19:03:25 -0700

    install file-loader and html-loader

--- __tests__/pigdice.test.js
+++ __tests__/pigdice.test.js
@@ -1,13 +1,14 @@
 import Player from './../src/pigdice.js';
 
 describe('Player', () => {
+    let player1;
 
   beforeEach(() => {
-    // player1 = new Player("Carnitas", 0, 0, true, false);
-  })
+    player1 = new Player("Carnitas", 0, 0, true, false);
+  });
 
   test('should correctly create a player object with respective key-values', () => {
-    let player1 = new Player("Carnitas", 0, 0, true, false);
+    // let player1 = new Player("Carnitas", 0, 0, true, false);
     expect(player1.name).toEqual("Carnitas");
     expect(player1.turnScore).toEqual(0);
     expect(player1.gameScore).toEqual(0);
@@ -16,7 +17,7 @@ describe('Player', () => {
   });
 
   test ('should roll random number and add to turn score if 2-6 or return 0 if 1', () => {
-    let player1 = new Player("Carnitas", 0, 0, true, false);
+    // let player1 = new Player("Carnitas", 0, 0, true, false);
     expect(player1.addScore()).toBeLessThan(7);
-  } )
+  });
 });
